d089e80e054bf8aab62bb04f512c987be9031b74
Nguyen Ngoc Manh Duy <duyngnm.299@gmail.com>
2023-01-31 14:52:53 +0700

    edit

--- server.js
+++ server.js
@@ -43,13 +43,7 @@ app.use("/user", userRoutes);
 app.use("/conversation", conversationRoutes);
 app.use("/message", messageRoutes);
 app.use("/transaction", transactionRoutes);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.DB_URL, { useNewUrlParser: true })
